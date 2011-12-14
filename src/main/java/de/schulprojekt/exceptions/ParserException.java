5151911787550be36b1bed6d709d1d1ab3253202
Patrick Trautmann <patrick.trautmann@gmail.com>
2011-12-14 20:47:43 +0100

    Es gibt nun auf Wunsch eine Parser Exception. Das Parser Interface throwt diese auch bereits :) Viel Spaß damit

---
+++ src/main/java/de/schulprojekt/exceptions/ParserException.java
@@ -0,0 +1,20 @@
+package de.schulprojekt.exceptions;
+
+/**
+ * Created by Patrick Trautmann
+ * <p/>
+ * Contact: patrick.trautmann@gmail.com
+ * Date: 14.12.11
+ * Time: 20:44
+ */
+public class ParserException extends Exception {
+
+    public ParserException() {
+
+    }
+
+    public ParserException(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/java/de/schulprojekt/model/parser/IParser.java
+++ src/main/java/de/schulprojekt/model/parser/IParser.java
@@ -2,9 +2,10 @@ package de.schulprojekt.model.parser;
 
 import de.schulprojekt.bean.parser.ParserParameterBean;
 import de.schulprojekt.entities.Recipe;
+import de.schulprojekt.exceptions.ParserException;
 
 public interface IParser {
 
-    public Recipe fetchRecipe(ParserParameterBean parameterBean);
+    public Recipe fetchRecipe(ParserParameterBean parameterBean) throws ParserException;
 
 }
