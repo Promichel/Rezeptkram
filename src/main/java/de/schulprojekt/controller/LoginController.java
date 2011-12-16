91041d469a99bb574e0e486a5eea0e40e09e86fb
Patrick Trautmann <patrick.trautmann@gmail.com>
2011-12-16 19:54:07 +0100

    Start coding login and registration

---
+++ src/main/java/de/schulprojekt/controller/LoginController.java
@@ -0,0 +1,11 @@
+package de.schulprojekt.controller;
+
+/**
+ * Created by Patrick Trautmann
+ * <p/>
+ * Contact: patrick.trautmann@gmail.com
+ * Date: 14.12.11
+ * Time: 23:09
+ */
+public class LoginController {
+}
---
+++ src/main/java/de/schulprojekt/controller/SignupController.java
@@ -0,0 +1,21 @@
+package de.schulprojekt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Patrick Trautmann
+ * <p/>
+ * Contact: patrick.trautmann@gmail.com
+ * Date: 14.12.11
+ * Time: 23:09
+ */
+public class SignupController {
+
+    private Logger logger = LoggerFactory.getLogger(SignupController.class);
+
+    public SignupController() {
+        logger.debug("Init SignupController()");
+    }
+
+}
