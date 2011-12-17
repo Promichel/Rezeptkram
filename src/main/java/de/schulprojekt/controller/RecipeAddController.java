608fe0b265741ffcc2071fc9b2ca2f8afc618f1c
Patrick Trautmann <patrick.trautmann@gmail.com>
2011-12-17 22:10:38 +0100

    Import Recipes from external site

--- src/main/java/de/schulprojekt/controller/RecipeAddController.java
+++ src/main/java/de/schulprojekt/controller/RecipeAddController.java
@@ -5,12 +5,18 @@ import de.schulprojekt.dao.RecipeDao;
 import de.schulprojekt.entities.Ingredient;
 import de.schulprojekt.entities.Recipe;
 import de.schulprojekt.entities.RecipeIngredient;
+import de.schulprojekt.exceptions.ParserException;
+import de.schulprojekt.model.parser.IParser;
+import de.schulprojekt.parsers.chefkoch.ChefkochParser;
+import de.schulprojekt.parsers.dasKochrezept.DasKochrezeptParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.component.html.HtmlSelectOneMenu;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.model.SelectItem;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +30,21 @@ public class RecipeAddController {
 
     private Logger logger = LoggerFactory.getLogger(RecipeAddController.class);
 
+    private int selectedParser;
+
     private List<SelectItem> units;
+    private List<SelectItem> availableParsers;
+
+    private IParser parser;
+    private String parserUrl;
+    private Recipe newRecipe;
 
     public RecipeAddController() {
         logger.debug("Init RecipeController");
 
         rab = new RecipeAddBean();
+        newRecipe = new Recipe();
+        newRecipe.setIngredients(new ArrayList<RecipeIngredient>());
         units = selectUnits();
     }
 
@@ -40,6 +55,68 @@ public class RecipeAddController {
         zUnits.add(new SelectItem("ml", "Milliliter"));
 
         return zUnits;
+    }
+
+    public void resetUserVars() {
+        logger.debug("Reset all vars to default value");
+        newRecipe = new Recipe();
+        newRecipe.setIngredients(new ArrayList<RecipeIngredient>());
+    }
+
+    public void onParserSelect(AjaxBehaviorEvent event) {
+
+        HtmlSelectOneMenu menu = (HtmlSelectOneMenu) event.getSource();
+        String input = menu.getValue().toString();
+
+
+        logger.debug("Selected parser with id {}", selectedParser);
+        if (selectedParser != 0) {
+            parseFromSite();
+        }
+
+    }
+
+    public String startParser() {
+
+        logger.debug("Start parsing");
+        if (selectedParser != 0 && parser != null) {
+            try {
+                newRecipe = parser.fetchRecipe(parserUrl);
+                logger.debug("Recipe name: {}", newRecipe.getName());
+            } catch (ParserException e) {
+                logger.error("An error occurred while parsing recipe with url {} from parser with url {}", new Object[]{parserUrl, selectedParser});
+            }
+
+        }
+
+        return null;
+    }
+
+    private void parseFromSite() {
+
+        logger.debug("Start selecting");
+
+        parser = null;
+
+        switch (selectedParser) {
+            case 1:
+                logger.debug("Set Chefkoch Parser");
+                parser = new ChefkochParser();
+                break;
+            case 2:
+                logger.debug("Set Das Kochrezept Parser");
+                parser = new DasKochrezeptParser();
+                break;
+        }
+
+    }
+
+    private void retrieveAvailableParsers() {
+        availableParsers = new ArrayList<SelectItem>();
+
+        availableParsers.add(new SelectItem(1, "Chefkoch.de"));
+        availableParsers.add(new SelectItem(2, "DasKochrezept.de"));
+
     }
 
     public String addRecipe() {
@@ -79,7 +156,7 @@ public class RecipeAddController {
 
             ingredient.setIngredient(article);
 
-            rab.getIngredients().add(ingredient);
+            newRecipe.getIngredients().add(ingredient);
 
             rab.setIngredientAmount(0);
             rab.setIngredientName("");
@@ -91,6 +168,41 @@ public class RecipeAddController {
         }
 
         return null;
+    }
+
+    public Recipe getNewRecipe() {
+        return newRecipe;
+    }
+
+    public void setNewRecipe(Recipe newRecipe) {
+        this.newRecipe = newRecipe;
+    }
+
+    public String getParserUrl() {
+        return parserUrl;
+    }
+
+    public void setParserUrl(String parserUrl) {
+        this.parserUrl = parserUrl;
+    }
+
+    public int getSelectedParser() {
+        return selectedParser;
+    }
+
+    public void setSelectedParser(int selectedParser) {
+        this.selectedParser = selectedParser;
+    }
+
+    public List<SelectItem> getAvailableParsers() {
+        if (availableParsers == null) {
+            retrieveAvailableParsers();
+        }
+        return availableParsers;
+    }
+
+    public void setAvailableParsers(List<SelectItem> availableParsers) {
+        this.availableParsers = availableParsers;
     }
 
     public List<SelectItem> getUnits() {
